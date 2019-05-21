341e15270d5b4769849eb90c714a8332eaaf2ed0
Ficky Mathebula <fmathebu@homechoice.co.za>
2019-05-21 15:00:04 +0200

    initial commit01

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! This is my first app.....");
+            Console.ReadKey();
+        }
+    }
+}
